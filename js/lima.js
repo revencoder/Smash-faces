59a19c479b5ddd24d06ad87484c9009837c18ba3
Cecilia Limay <mcecilia.limay@gmail.com>
2016-05-24 16:43:34 -0500

    Listo

---
+++ js/lima.js
@@ -0,0 +1,56 @@
+var lima = [
+			{image: "Alma Patricia Jimenez.JPG",
+			name:"Alma"},
+
+			{image:"Analy Miranda.JPG",
+			name:"Analy"},
+
+			{image:"Beatriz Quesadas.JPG",
+			 name:"Beatriz"},
+
+			{image:"Daniela Belem García.JPG",
+			 name:"Daniela"},
+
+			{image:"Evelyn Vázquez.JPG",
+			 name:"Evelyn"},
+
+			{image:"Grissel Coutiño.JPG",
+			 name:"Grissel"},
+
+			{image:"Johana Alexa Vargas.JPG",
+			 name: "Johana"},
+
+			{image:"Juana Ofelia García.JPG",
+			 name:"Juana"},
+
+			{image:"Karen Cruz Heredia.JPG",
+			 name:"Karen"},
+
+			{image:"Karla Monica Llerenas.JPG",
+			 name:"Karla"},
+
+			{image:"Leslie Anigail Vazquez.JPG",
+			 name:"Leslie"},
+
+			{image:"Milca Sarai Del Angel.JPG",
+			 name:"Milca"},
+
+			{image:"Nayeli Carbajal.JPG",
+			 name:"Nayeli"},
+
+			{image:"Reyna Hernández.JPG",
+			 name:"Reyna"},
+
+			{image:"Ruth López.JPG",
+			 name:"Ruth"},
+
+			{image:"Sandra Bollo.JPG",
+			 name:"Sandra"},
+
+			{image:"Vianey Tavatha Moreno.JPG",
+			 name:"Vianey"},
+
+			{image:"Cecilia Guadalupe Ampudia .JPG",
+			 name:"Cecilia"}
+
+			];
